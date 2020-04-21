e3c25c248d407e0aa1f911d15da19f3b28d2346b
Neelu <neelu.rsa@gmail.com>
2020-04-21 12:23:16 +0100

    Confirm current password

--- jghsdofe/users/forms.py
+++ jghsdofe/users/forms.py
@@ -4,6 +4,7 @@ from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import InputRequired, Email, ValidationError, EqualTo, Length
 from jghsdofe.models import User
+from jghsdofe import bcrypt
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired()])
@@ -25,6 +26,11 @@ class SignupForm(FlaskForm):
 
 
 class ChangePasswordForm(FlaskForm):
-    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, message='Password must be at least 8 characters long.')])
-    confirm_password = PasswordField('Confirm Password', validators=[InputRequired(), EqualTo('password', message='Password and Confirm Password must be identical. Note that passwords are case-sensitive.')])
+    current_password = PasswordField('Current Password', validators=[InputRequired()])
+    password = PasswordField('New Password', validators=[InputRequired(), Length(min=8, message='Password must be at least 8 characters long.')])
+    confirm_password = PasswordField('Confirm New Password', validators=[InputRequired(), EqualTo('password', message='Password and Confirm Password must be identical. Note that passwords are case-sensitive.')])
     submit = SubmitField('Change Password')
+
+    def validate_current_password(self, current_password):
+        if not bcrypt.check_password_hash(current_user.password, current_password.data):
+            raise ValidationError('Current password incorrect. Please try again. If you have forgotten it please email neelu.rsa@gmail.com for assistance.')
